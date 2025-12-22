174d645bafbd690b66f1091c8d17b7601115d676
Rimonkumar <22-46957-1@student.aiub.edu>
2025-12-22 23:05:15 +0600

    implement login method

--- src/Component/SignIn.jsx
+++ src/Component/SignIn.jsx
@@ -1,59 +1,78 @@
-import React, { use, useState } from 'react';
-import {  Link } from 'react-router'; // Added useNavigate
+import React, { use,  useState } from 'react';
+import { Link } from 'react-router';
 import { AuthContext } from '../context/AuthContext';
 
 const SignIn = () => {
     const { signInUser } = use(AuthContext);
     const [error, setError] = useState('');
-    
 
     const handleSignIn = (e) => {
         e.preventDefault();
-        setError('');
-
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email,password)
 
-        // Use the function from Context
+        setError(''); // clear previous error
+
         signInUser(email, password)
-            .then((userCredential) => {
-                console.log("Logged In:", userCredential.user);
-                e.target.reset(); // Clear the form
+            .then(result => {
+                console.log("Logged In:", result.user);
+                alert('login successfuly')
             })
-            .catch((error) => {
-                // Friendly error messages
-                if (error.code === 'auth/invalid-credential') {
-                    setError("Invalid email or password.");
-                } else {
-                    setError(error.message);
-                }
+            .catch(err => {
+                console.error(err);
+                setError(err.message);
             });
     };
 
     return (
         <div className='flex justify-center min-h-screen items-center bg-base-200'>
-            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
+            <div className="card bg-base-100 w-full max-w-sm shadow-2xl">
                 <div className="card-body p-10">
-                    <h1 className="text-3xl font-bold mb-5 text-center">Login Now</h1>
+                    <h1 className="text-3xl font-bold mb-5 text-center">
+                        Login Now
+                    </h1>
 
-                    <form onSubmit={handleSignIn} className="fieldset">
+                    <form onSubmit={handleSignIn}>
                         <label className="label">Email</label>
-                        <input type="email" name="email" className="input" placeholder="Email" required />
+                        <input
+                            type="email"
+                            name="email"
+                            className="input input-bordered"
+                            placeholder="Email"
+                            required
+                        />
 
-                        <label className="label">Password</label>
-                        <input type="password" name="password" className="input" placeholder="Password" required />
+                        <label className="label mt-3">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            className="input input-bordered"
+                            placeholder="Password"
+                            required
+                        />
 
-                        <div><a className="link link-hover text-sm">Forgot password?</a></div>
-                        
-                        {error && <p className='text-red-500 text-sm mt-2 font-medium'>{error}</p>}
+                        <div className="mt-2">
+                            <a className="link link-hover text-sm">
+                                Forgot password?
+                            </a>
+                        </div>
 
-                        <button type='submit' className="btn btn-neutral mt-4">Login</button>
+                        {error && (
+                            <p className='text-red-500 text-sm mt-2 font-medium'>
+                                {error}
+                            </p>
+                        )}
+
+                        <button type='submit' className="btn btn-neutral mt-4 w-full">
+                            Login
+                        </button>
                     </form>
 
                     <p className='mt-3 text-center'>
-                        Don’t Have An Account?  
-                        <Link className='text-secondary font-bold ml-1' to="/signup">SignUp!</Link>
+                        Don’t Have An Account?
+                        <Link className='text-secondary font-bold ml-1' to="/signup">
+                            SignUp!
+                        </Link>
                     </p>
                 </div>
             </div>
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase/fireBase.init';
 
 const AuthProvider = ({ children }) => {
@@ -10,9 +10,14 @@ const AuthProvider = ({ children }) => {
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
+
+    const signInUser = (email, password) =>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
     const userInfo = {
 
         createUser,
+        signInUser,
     }
 
     return (
